86e0f295bb826c98390fc6d992e3051af48a2bd2
Simeon Radivoev <simeonradivoev@gmail.com>
2024-02-11 14:37:22 +0200

    fix: Property drawer should now work with older unity versions

--- Editor/NamedReferencePropertyDrawer.cs
+++ Editor/NamedReferencePropertyDrawer.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using UnityEditor;
 using UnityEditor.UIElements;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace NamedReferences.Editor
@@ -9,6 +10,17 @@ namespace NamedReferences.Editor
     [CustomPropertyDrawer(typeof(NamedReferenceAttribute))]
     public class NamedReferencePropertyDrawer : PropertyDrawer
     {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var attribute = fieldInfo.GetCustomAttribute<NamedReferenceAttribute>();
+            EditorGUI.ObjectField(position, property, EditorGUIUtility.TrTempContent(attribute.IsDirect ? $"{attribute.Name} (Direct)" : attribute.Name));
+        }
+
         #region Overrides of PropertyDrawer
 
         public override VisualElement CreatePropertyGUI(SerializedProperty property)
